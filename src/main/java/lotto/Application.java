1a27b195d1c5dcb388cb63be981fb7a6d7c57e27
eeeseohyun <eeeseohyun@gmail.com>
2023-07-04 22:57:22 +0900

    로또구입 금액 입력받기 - 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        String price = Console.readLine();
+        int price = Integer.parseInt(Console.readLine());
 
         List<Integer> numbers = new ArrayList<>();
 
@@ -19,7 +19,7 @@ public class Application {
         }
 
         int bonusNum = Integer.parseInt(Console.readLine());
-
+        
 
     }
 }
