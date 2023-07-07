8d47cb94ce6377b5633aa3112fc83b547415947d
Hoya324 <gangho324@naver.com>
2023-07-07 16:54:19 +0900

    Feat: Message Enum 구현
    입출력, 에러에 사용되는 메세지를 Enum에 정리

---
+++ src/main/java/lotto/Message/OutputMessage.java
@@ -0,0 +1,14 @@
+package lotto.Message;
+
+public enum OutputMessage {
+    LOTTO_RESULT("%d개 일치%s (%s원) - %d개"),
+    RATE_OF_LOTTERY_WINNINGS("총 수익률은 %s%%입니다.");
+
+    private final String message;
+    OutputMessage(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+}
