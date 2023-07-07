618f368f031372dfdea00620d6ca105b71ea8d2d
Hoya324 <gangho324@naver.com>
2023-07-07 16:58:21 +0900

    Test: 테스트 코드 추가
    로또 번호가 1에서 45 사이가 아니면 예외가 발생하는지 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,4 +24,11 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1에서 45 사이가 아니면 예외가 발생한다.")
+    @Test
+    void 로또번호_1_45_사이인지_판단() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 90)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
