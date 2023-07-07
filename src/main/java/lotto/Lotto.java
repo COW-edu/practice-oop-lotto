ef0b2bfb0c5715f821d3ebe82c5c0e70183cc7cd
Hoya324 <gangho324@naver.com>
2023-07-07 16:49:23 +0900

    Feat: 에러처리 및 프로그램 실행
    로또번호는 1~45 사이에 속하는 값이다.
    구매금액은 1000의 자리 수이다.
    구매금액은 1000 이상의 수이다.
    로또 번호는 6개이다.
    각각의 로또 번호는 중복되지 않는다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,60 @@
 package lotto;
 
+import lotto.Message.InputMessage;
+import lotto.view.InputView;
+import lotto.Message.LottoExceptionMessage;
+import lotto.Message.OutputMessage;
+import lotto.view.OutputView;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
-
     private void validate(List<Integer> numbers) {
+        isNotEnoughSize(numbers);
+        isDuplicate(numbers);
+        isInOneToFortyFive(numbers);
+    }
+    private void isNotEnoughSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LottoExceptionMessage.IS_NOT_ENOUGH_LOTTO_NUMBER.getMessage());
         }
     }
-
-    // TODO: 추가 기능 구현
+    private void isDuplicate(List<Integer> numbers) {
+        Set<Integer> numbersSet = new HashSet<>(numbers);
+        if (numbersSet.size() != numbers.size()) {
+            throw new IllegalArgumentException(LottoExceptionMessage.IS_DUPLICATE_LOTTO_NUMBER.getMessage());
+        }
+    }
+    private void isInOneToFortyFive(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(LottoExceptionMessage.LOTTO_NUMBER_IN_1_45.getMessage());
+            }
+        }
+    }
+    public static void run() {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        try {
+            int purchasedLottoCounts = inputView.inputPurchaseAmount(InputMessage.INPUT_PURCHASE_AMOUNT);
+            outputView.outPutLottoList(purchasedLottoCounts, InputMessage.PURCHASED_LOTTO_COUNTS);
+            List<Integer> winningNumbers = inputView.inputWinningNumbers(InputMessage.INPUT_WIN_LOTTO_NUMBERS);
+            Lotto lotto = new Lotto(winningNumbers);
+            lotto.numbers = inputView.inputBonusNumber(InputMessage.INPUT_BONUS_NUMBER);
+            lotto.isDuplicate(lotto.numbers);
+            String totalWinningMoney = outputView.outPutLottoTotalResult(OutputMessage.LOTTO_RESULT, purchasedLottoCounts);
+            outputView.rateOfLotteryWinnings(OutputMessage.RATE_OF_LOTTERY_WINNINGS, totalWinningMoney);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
