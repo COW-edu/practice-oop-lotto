0ebf5cbc6c9ab39916622b3511310319406e881d
eeeseohyun <eeeseohyun@gmail.com>
2023-07-09 19:17:31 +0900

    리펙토링

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,21 +9,28 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("금액을 입력해주세요");
         int price = receivePrice();
+        System.out.println("로또 개수:"+price);
+
 
         List<Integer> numbers = new ArrayList<>();
-        for(int i=0; i<5; i++){
+        for(int i=0; i<6; i++){
+            System.out.println((i+1)+"번째 로또 당첨 번호를 입력해주세요");
             int number = Integer.parseInt(Console.readLine());
             numbers.add(number);
         }
+        System.out.println("로또 당첨 번호는" +numbers+"입니다.");
+        System.out.println("보너스 당첨 번호를 입력해주세요");
         int bonusNum = Integer.parseInt(Console.readLine());
+        System.out.println("보너스 당첨 번호는" +bonusNum+"입니다.");
         Lotto lotto = new Lotto(numbers);
 
-        List<List<Integer>> usernum =new ArrayList<List<Integer>>();
+        List<List<Integer>> userNum =new ArrayList<>();
         for(int i=0; i<price; i++) {
-            usernum.add(create());
+            userNum.add(create());
         }
-
+        System.out.println("발행한 로또 번호입니다\n"+userNum);
     }
     private static int receivePrice(){
         int price = Integer.parseInt(Console.readLine());
@@ -37,7 +44,6 @@ public class Application {
         price=price/1000;
         return price;
     }
-
     private static List<Integer> create(){
         List<Integer> numbers = new ArrayList<>();
         numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
