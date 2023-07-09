4e38f2d3ea6498da839e572a24fe1d1dba1e7aca
taewook02 <taewook02@mju.ac.kr>
2023-07-09 22:45:51 +0900

    feat: ViewText에 내용 추가

--- src/main/java/lotto/view/ViewText.java
+++ src/main/java/lotto/view/ViewText.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 public enum ViewText {
     INPUT_PURCHASE_PRICE("구입 금액을 입력해 주세요."),
+    OUTPUT_PURCHASE_AMOUNT("개를 구매했습니다."),
     INPUT_WIN_NUMBER("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     OUTPUT_WIN_STATISTICS_TITLE("당첨 통계"),
