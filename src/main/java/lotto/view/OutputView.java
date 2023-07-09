c2f4d1ee94e11a89ba1edf8db89e290a051ae503
Hoya324 <gangho324@naver.com>
2023-07-09 18:30:12 +0900

    test: private 메서드 위치 변경
    private 메서드 위치 public 아래로 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,53 +1,53 @@
 package lotto.view;
 
+import lotto.controller.LottoController;
+import lotto.domain.Lotto;
 import lotto.message.InputMessage;
 import lotto.message.OutputMessage;
 import lotto.message.ResultMessage;
-import lotto.controller.LottoController;
 
 import java.util.List;
 
 public class OutputView {
-    private final int ONE_HUNDRED = 100;
-    private final int ONE_THOUSAND = 1000;
+    private final int CHANGE_PERCENT = 100;
+    private final int CHANGE_PURCHASE_MONEY = 1000;
     private final int RESULT_INDEX_THREE = 3;
     private final int RESULT_INDEX_SEVEN = 7;
 
-    private final LottoController lottoController = new LottoController();
+    private final LottoController lottoController;
 
-    public void outPutLottoList(int purchasedLottoCounts, InputMessage message) {
-        System.out.println(String.format(message.getMessage(), purchasedLottoCounts));
-        List<List<Integer>> lottoList = lottoController.createLottoList(purchasedLottoCounts);
+    public OutputView(LottoController lottoController) {
+        this.lottoController = lottoController;
+    }
+
+    public void outPutLottoList(int purchasedLottoCounts) {
+        System.out.println(String.format(InputMessage.PURCHASED_LOTTO_COUNTS.getMessage(), purchasedLottoCounts));
+        List<Lotto> lottoList = lottoController.createLottoList(purchasedLottoCounts);
+        StringBuilder totalStringLottoList = new StringBuilder();
         for (int index = 0; index < purchasedLottoCounts; index++) {
-            System.out.println(lottoList.get(index));
+            totalStringLottoList.append(lottoList.get(index).getNumbers()).append("\n");
         }
+        System.out.println(totalStringLottoList.toString());
     }
 
-    private String outPutResultComment(OutputMessage outputMessage, ResultMessage resultMessage, int [] winningLottoCountList, int correctNumberCount) {
-        return  String.format(outputMessage.getMessage(), resultMessage.getWinningCount(),
-                resultMessage.getBonusComment(), resultMessage.getWinningMoney(), winningLottoCountList[correctNumberCount-3]);
-    }
-
-    private int calculateTotalWinningMoney(ResultMessage resultMessage, int [] winningLottoCountList, int correctNumberCount) {
-        int winningMoney = 0;
-        winningMoney += Integer.parseInt(resultMessage.getWinningMoney().replaceAll(",", "")) * winningLottoCountList[correctNumberCount-3];
-        return winningMoney;
-    }
-
-    public String outPutLottoTotalResult(OutputMessage outputMessage, int purchasedLottoCounts) {
+    public String outPutLottoTotalResult(int purchasedLottoCounts) {
         ResultMessage resultMessage;
         double totalWinningMoney = 0.0;
         System.out.println("\n당첨 통계\n---");
         int [] winningLottoCountList = lottoController.countWinningNumber(purchasedLottoCounts);
         for (int correctNumberCount = RESULT_INDEX_THREE; correctNumberCount <= RESULT_INDEX_SEVEN; correctNumberCount++) {
             resultMessage = ResultMessage.valueOf(correctNumberCount);
-            System.out.println(outPutResultComment(outputMessage, resultMessage, winningLottoCountList, correctNumberCount));
-            totalWinningMoney += calculateTotalWinningMoney(resultMessage, winningLottoCountList, correctNumberCount);
+            System.out.println(outPutResultComment(resultMessage, winningLottoCountList, correctNumberCount));
+            totalWinningMoney += lottoController.calculateTotalWinningMoney(resultMessage, winningLottoCountList, correctNumberCount);
         }
-        return String.format("%.1f", totalWinningMoney/(purchasedLottoCounts*ONE_THOUSAND)*ONE_HUNDRED);
+        return String.format("%.1f", totalWinningMoney/(purchasedLottoCounts*CHANGE_PURCHASE_MONEY)*CHANGE_PERCENT);
+    }
+    private String outPutResultComment(ResultMessage resultMessage, int [] winningLottoCountList, int correctNumberCount) {
+        return  String.format(OutputMessage.LOTTO_RESULT.getMessage(), resultMessage.getWinningCount(),
+                resultMessage.getBonusComment(), resultMessage.getWinningMoney(), winningLottoCountList[correctNumberCount-3]);
     }
 
-    public void rateOfLotteryWinnings(OutputMessage outputMessage, String totalWinningMoney) {
-        System.out.println(String.format(outputMessage.getMessage(), totalWinningMoney));
+    public void outPutRateOfLotteryWinnings(String totalWinningMoney) {
+        System.out.println(String.format(OutputMessage.RATE_OF_LOTTERY_WINNINGS.getMessage(), totalWinningMoney));
     }
 }
