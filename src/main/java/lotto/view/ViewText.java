e0a211c4e44605917308c97b6bf109fee1def40a
taewook02 <taewook02@mju.ac.kr>
2023-07-09 22:15:59 +0900

    feat: ViewText에 내용 추가

--- src/main/java/lotto/view/ViewText.java
+++ src/main/java/lotto/view/ViewText.java
@@ -5,7 +5,15 @@ public enum ViewText {
     INPUT_WIN_NUMBER("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     OUTPUT_WIN_STATISTICS_TITLE("당첨 통계"),
-    OUTPUT_DIVISION_LINE("---");
+    OUTPUT_DIVISION_LINE("---"),
+    OUTPUT_THREE_ACCORD_NUMBERS("3개 일치 (5,000원) - "),
+    OUTPUT_FOUR_ACCORD_NUMBERS("4개 일치 (50,000원) - "),
+    OUTPUT_FIVE_ACCORD_NUMBERS("5개 일치 (1,500,000원) - "),
+    OUTPUT_FIVE_ACCORD_NUMBERS_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    OUTPUT_SIX_ACCORD_NUMBERS("6개 일치 (2,000,000,000원) - "),
+    OUTPUT_ACCORD_NUMBERS_TAIL("개"),
+    OUTPUT_TOTAL_PROFIT_RATE_HEAD("총 수익률은 "),
+    OUTPUT_TOTAL_PROFIT_RATE_TAIL("%입니다.");
 
     private final String data;
     ViewText(String data) {
