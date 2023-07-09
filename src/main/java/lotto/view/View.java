f8c8f924bcf627a1338551d8cb82c6a41256a50d
taewook02 <taewook02@mju.ac.kr>
2023-07-09 22:46:09 +0900

    feat: View 클래스에 화면 출력 관련 메소드 추가

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -4,10 +4,25 @@ import camp.nextstep.edu.missionutils.Console;
 
 import lotto.model.ErrorMessage;
 import lotto.controller.Controller;
+import lotto.model.Wallet;
 
 import java.util.List;
 
 public class View {
+    public void println(String message) {
+        System.out.println(message);
+    }
+
+    public void print(String message) {
+        System.out.print(message);
+    }
+    public void print(int number) {
+        System.out.print(number);
+    }
+    public void print(Double number) {
+        System.out.print(number);
+    }
+
     public String readString() {
         String res = Console.readLine();
 
@@ -18,12 +33,17 @@ public class View {
         return res;
     }
 
+    public void getPurchasePrice(Wallet wallet) {
+        Controller controller = new Controller();
+        controller.handlePurchasePrice(wallet, readString());
+    }
+
     public List<Integer> getWinNumber() {
         Controller controller = new Controller();
         return controller.handleWinNumber(readString());
     }
 
-    public int getBonusNumber(String bonusNumberLine) {
+    public int getBonusNumber() {
         Controller controller = new Controller();
         return controller.handleBonusNumber(readString());
     }
