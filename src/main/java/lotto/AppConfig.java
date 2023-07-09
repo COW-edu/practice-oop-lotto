85821ec3be10fa53a6cb4c3f45d5b7ffcf483a3c
Hoya324 <gangho324@naver.com>
2023-07-09 17:41:56 +0900

    feat: 전체 객체 구성 관리자 추가
    AppConfig를 통해 전체 객체를 관리하고, 의존관계를 주입합니다.

---
+++ src/main/java/lotto/AppConfig.java
@@ -0,0 +1,49 @@
+package lotto;
+
+import lotto.controller.LottoController;
+import lotto.controller.LottoControllerImpl;
+import lotto.model.LottoNumberRepository;
+import lotto.model.LottoNumberRepositoryImpl;
+import lotto.model.LottoResultRepository;
+import lotto.model.LottoResultRepositoryImpl;
+import lotto.model.WinningLottoNumberRepository;
+import lotto.model.WinningLottoNumberRepositoryImpl;
+import lotto.service.LottoService;
+import lotto.service.LottoServiceImpl;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class AppConfig {
+
+    public LottoService lottoService() {
+        return new LottoServiceImpl(
+                lottoNumberRepository(),
+                winningLottoNumberRepository(),
+                lottoResultRepository());
+    }
+
+    public InputView inputView() {
+        return new InputView(lottoController());
+    }
+
+    public OutputView outputView() {
+        return new OutputView(lottoController());
+    }
+
+    public LottoController lottoController() {
+        return new LottoControllerImpl(lottoService());
+    }
+
+    public LottoNumberRepository lottoNumberRepository() {
+        return new LottoNumberRepositoryImpl();
+    }
+
+    public WinningLottoNumberRepository winningLottoNumberRepository() {
+        return new WinningLottoNumberRepositoryImpl();
+    }
+
+    public LottoResultRepository lottoResultRepository() {
+        return new LottoResultRepositoryImpl();
+    }
+
+}
