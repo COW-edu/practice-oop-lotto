e0298abb9c5d283d02a249e415fb2ef1e227da89
eeeseohyun <eeeseohyun@gmail.com>
2023-07-09 20:33:18 +0900

    보너스 볼 계산

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,37 +16,48 @@ public class Lotto {
         }
     }
 
-    public void compare(List<List<Integer>> UserNum) {
+    public void compare(List<List<Integer>> UserNum,int bonus) {
         System.out.println("실행중");
         int result[]=new int[5];
         Arrays.fill(result,0);
         for(int i=0; i<UserNum.size(); i++){
             List<Integer> useList= UserNum.get(i);
-            result = count(useList,result);
+            result = count(useList,result,bonus);
         }
         printResult(result);
     }
 
     private void printResult(int[] result) {
         int price[]= {5000,50000,1500000,30000000,2000000000};
-        for(int i=0; i<result.length;i++){
+        for(int i=0; i<result.length-2;i++){
             System.out.println(i+3+"개 일치 ("+price[i]+"원) - "+result[i]+"개");
         }
+        System.out.println("5개 일치, 보너스 볼 일치 ("+price[3]+"원) - "+result[3]+"개");
+        System.out.println("6개 일치 ("+price[4]+"원) - "+result[4]+"개");
+
     }
 
-    private int[] count(List<Integer> useList, int[] result) {
+    private int[] count(List<Integer> useList, int[] result, int bonus) {
         List<Integer> sumList = new ArrayList<>(12);
         sumList.addAll(useList);
         sumList.addAll(numbers);
         Set<Integer> numSet=new HashSet<>(sumList);
         int totalCount=12;
         int count= totalCount- numSet.size();
-        result =calculator(count,result);
+        boolean bonusCheck=false;
+        if(useList.contains(bonus)){
+            bonusCheck=true;
+        }
+        result =calculator(count,result,bonusCheck);
         return result;
     }
 
-    private int[] calculator(int count, int[] result){
+    private int[] calculator(int count, int[] result, boolean bonusCheck){
         if(count<3){
+            return result;
+        }
+        if(count==5&&bonusCheck==true){
+            result[3]=result[3]+1;
             return result;
         }
         result[count-3]=result[count-3]+1;
