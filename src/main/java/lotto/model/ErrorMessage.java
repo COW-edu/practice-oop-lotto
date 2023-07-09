118364a0a655c806c2459c584e45b7610dc6ce4c
taewook02 <taewook02@mju.ac.kr>
2023-07-09 16:25:48 +0900

    feat: ErrorMessage 열거형 추가
    에러메시지 열거형

---
+++ src/main/java/lotto/model/ErrorMessage.java
@@ -0,0 +1,17 @@
+package lotto.model;
+
+public enum ErrorMessage {
+    ERROR_NULL_INPUT("[ERROR] null 값이 입력되었습니다."),
+    ERROR_NOT_INTEGER_INPUT("[ERROR] 정수가 아닌 값이 입력되었습니다."),
+    ERROR_WIN_NUMBER_LENGTH("[ERROR] 로또 번호는 6개여야 합니다."),
+    ERROR_PURCHASE_PRICE_UNIT("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
+
+    private final String data;
+    ErrorMessage(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+}
