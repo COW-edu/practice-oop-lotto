46179cd56cc0289acd725a525eca1587d9032a60
Hoya324 <gangho324@naver.com>
2023-07-15 14:43:50 +0900

    refactor: 변수명 변경
    LOTTO_NUMBER_IN_1_45 -> LOTTO_INCORRECT_RANGE

--- src/main/java/lotto/message/LottoExceptionMessage.java
+++ src/main/java/lotto/message/LottoExceptionMessage.java
@@ -5,7 +5,7 @@ public enum LottoExceptionMessage {
   IS_NOT_ENOUGH_LOTTO_NUMBER("[ERROR] 6개의 로또 번호를 입력해주세요"),
   UNDER_1000("[ERROR] 1000원보다 많은 금액을 입력해주세요."),
   IS_NOT_DIVIDED_1000("[ERROR] 1000으로 나누어 떨어지는 금액을 입력해주세요."),
-  LOTTO_NUMBER_IN_1_45("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+  LOTTO_INCORRECT_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
   IS_DUPLICATE_LOTTO_NUMBER("[ERROR] 로또 번호는 중복될 수 없습니다."),
   ;
 
