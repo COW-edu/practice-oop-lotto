7e84de9fe4f79a67e89d358a252a11a2925d942c
Hoya324 <gangho324@naver.com>
2023-07-15 17:53:16 +0900

    test: 예외 메세지 변경
    ApplicationTest 예외 메세지 변경

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.message.LottoExceptionMessage;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,8 +11,6 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest extends NsTest {
-
-  private static final String ERROR_MESSAGE = "[ERROR]";
 
   @Test
   void 기능_테스트() {
@@ -51,7 +50,7 @@ class ApplicationTest extends NsTest {
   void 예외_테스트() {
     assertSimpleTest(() -> {
       runException("1000j");
-      assertThat(output()).contains(ERROR_MESSAGE);
+      assertThat(output()).contains(LottoExceptionMessage.NOT_INTEGER.getMessage());
     });
   }
 
