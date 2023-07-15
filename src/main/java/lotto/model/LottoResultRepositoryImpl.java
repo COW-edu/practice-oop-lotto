d94eaf6f61cdddbb6f51b6bef55c66f5b2a5abbd
Hoya324 <gangho324@naver.com>
2023-07-15 17:59:18 +0900

    refactor: 변수명 변경
    의미있는 이름으로 변수명 변경

--- src/main/java/lotto/model/LottoResultRepositoryImpl.java
+++ src/main/java/lotto/model/LottoResultRepositoryImpl.java
@@ -2,14 +2,14 @@ package lotto.model;
 
 public class LottoResultRepositoryImpl implements LottoResultRepository {
 
-  private static final int MIN_LOTTERY_WINNING_COUNT = 3;
-  private static final int CHANGE_WINNING_COUNT_TO_INDEX = 3;
+  private static final int RANK_1 = 3;
+  private static final int INDEXING = 3;
   private final int[] lottoResult = new int[5];
 
   @Override
   public void saveLottoResult(int winningCount) {
-    boolean isWinning = (winningCount >= MIN_LOTTERY_WINNING_COUNT);
-    int lottoResultIndex = winningCount - CHANGE_WINNING_COUNT_TO_INDEX;
+    boolean isWinning = (winningCount >= RANK_1);
+    int lottoResultIndex = winningCount - INDEXING;
     if (isWinning) {
       lottoResult[lottoResultIndex] += 1;
     }
