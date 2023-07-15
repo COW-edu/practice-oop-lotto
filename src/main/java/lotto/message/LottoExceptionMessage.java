d481d8ba279ef8b47aa5d13eb713a72dc260133c
Hoya324 <gangho324@naver.com>
2023-07-15 17:55:55 +0900

    refactor: 변수명 변경
    예외 메세지 enum 변수명 변경(통일성을 위해)

--- src/main/java/lotto/message/LottoExceptionMessage.java
+++ src/main/java/lotto/message/LottoExceptionMessage.java
@@ -1,10 +1,10 @@
 package lotto.message;
 
 public enum LottoExceptionMessage {
-  NOT_CORRECT_PURCHASE_AMOUNT("[ERROR] 잘못된 입력값입니다."),
+  NOT_INTEGER("[ERROR] 정수형으로 입력해주세요."),
   IS_NOT_ENOUGH_LOTTO_NUMBER("[ERROR] 6개의 로또 번호를 입력해주세요"),
-  UNDER_1000("[ERROR] 1000원보다 많은 금액을 입력해주세요."),
-  IS_NOT_DIVIDED_1000("[ERROR] 1000으로 나누어 떨어지는 금액을 입력해주세요."),
+  LESS_THAN_MINIMUM("[ERROR] 1000원보다 많은 금액을 입력해주세요."),
+  NOT_DIVIDED_UNIT("[ERROR] 1000으로 나누어 떨어지는 금액을 입력해주세요."),
   LOTTO_INCORRECT_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
   IS_DUPLICATE_LOTTO_NUMBER("[ERROR] 로또 번호는 중복될 수 없습니다."),
   ;
