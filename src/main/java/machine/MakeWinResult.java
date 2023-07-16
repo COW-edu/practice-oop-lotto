419d7ed1cd5800a38e68ee267c5154eb25cf8243
ko seon je <kosunje1344@naver.com>
2023-07-16 22:02:08 +0900

    기능에 따른 클래스 분리

---
+++ src/main/java/machine/MakeWinResult.java
@@ -0,0 +1,48 @@
+package machine;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import model.lotto.Lotto;
+import model.lotto.LottoData;
+import model.win.WinLottoCount;
+
+public class MakeWinResult {
+
+  public List<Lotto> makeLottoList(List<Lotto> lottos, int countOfLotto) {
+    for(int i=0; i< countOfLotto; i++){
+      List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(LottoData.START_LOTTO_NUMBER,
+          LottoData.END_LOTTO_NUMBER,
+          LottoData.COUNT_OF_LOTTO_NUMBER);
+      lottos.add(new Lotto(lottoNumbers));
+    }
+    return lottos;
+  }
+
+  public Map<String,Integer> makeWinLottoResultData(int[] equalCounts, boolean[] bonusCounts,
+      int countOfLotto) {
+    Map<String,Integer> winCountTemp = new HashMap<>();
+    for(WinLottoCount wincount : WinLottoCount.values()){
+      double winNumber =Double.parseDouble(wincount.getCount());
+      winCountTemp.put(wincount.getCount(),countWin(winNumber, equalCounts, bonusCounts,countOfLotto));
+    }
+    return winCountTemp;
+  }
+
+  private int countWin(double winNumber, int[] equalCounts, boolean[] bonusCounts, int countOfLotto){
+    int count=0;
+    boolean isInteger = (winNumber%1==0);
+    for(int i = 0; i< countOfLotto; i++){
+      if(!isInteger&&equalCounts[i]==(int)winNumber&&bonusCounts[i]){
+        count++;
+        continue;
+      }
+      if(isInteger&&equalCounts[i]==winNumber&&bonusCounts[i])continue;
+      if(equalCounts[i]==winNumber){
+        count++;
+      }
+    }
+    return count;
+  }
+}
--- src/main/java/model/lotto/LottoStore.java
+++ src/main/java/model/lotto/LottoStore.java
@@ -1,30 +1,21 @@
 package model.lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import model.win.WinLottoCount;
+import machine.MakeWinResult;
 import model.win.WinLottoResult;
 
 public class LottoStore {
   private static final List<Lotto> lottos = new ArrayList<>();
   private int countOfLotto;
+  private MakeWinResult makeWinResult;
+  public LottoStore(){
+    makeWinResult=new MakeWinResult();
+  }
 
   public List<Lotto> makeLottoList(int payMoney){
     countOfLotto = lottoSizeCount(payMoney);
-    for(int i=0; i< countOfLotto; i++){
-      List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(LottoData.START_LOTTO_NUMBER,
-          LottoData.END_LOTTO_NUMBER,
-          LottoData.COUNT_OF_LOTTO_NUMBER);
-      lottos.add(makeLotto(lottoNumbers));
-    }
-    return lottos;
-  }
-
-  public Lotto makeLotto(List<Integer> LottoNum){
-    return new Lotto(LottoNum);
+    return makeWinResult.makeLottoList(lottos,countOfLotto);
   }
 
   public int lottoSizeCount(int payMoney){
@@ -36,32 +27,7 @@ public class LottoStore {
   }
 
   public WinLottoResult makeWinLottoResult(int[] equalCounts, boolean[] bonusCounts){
-    return new WinLottoResult(makeWinLottoResultData(equalCounts,bonusCounts));
-  }
-
-  private Map<String,Integer> makeWinLottoResultData(int[] equalCounts, boolean[] bonusCounts) {
-    Map<String,Integer> winCountTemp = new HashMap<>();
-    for(WinLottoCount wincount : WinLottoCount.values()){
-      double winNumber =Double.parseDouble(wincount.getCount());
-      winCountTemp.put(wincount.getCount(),countWin(winNumber, equalCounts, bonusCounts));
-    }
-    return winCountTemp;
-  }
-
-  private int countWin(double winNumber, int[] equalCounts, boolean[] bonusCounts){
-    int count=0;
-    boolean isInteger = (winNumber%1==0);
-    for(int i = 0; i< countOfLotto; i++){
-      if(!isInteger&&equalCounts[i]==(int)winNumber&&bonusCounts[i]){
-        count++;
-        continue;
-      }
-      if(isInteger&&equalCounts[i]==winNumber&&bonusCounts[i])continue;
-      if(equalCounts[i]==winNumber){
-        count++;
-      }
-    }
-    return count;
+    return new WinLottoResult(makeWinResult.makeWinLottoResultData(equalCounts,bonusCounts,countOfLotto));
   }
 
 }
