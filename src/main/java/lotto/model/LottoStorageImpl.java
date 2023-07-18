6211597066973021b6ec31fc433cfdd5500b722e
taewook02 <taewook02@mju.ac.kr>
2023-07-18 23:02:29 +0900

    feat: LottoStorageImpl 클래스 추가

---
+++ src/main/java/lotto/model/LottoStorageImpl.java
@@ -0,0 +1,18 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoStorageImpl implements LottoStorage {
+    private final List<Lotto> lottoList = new ArrayList<>();
+
+    @Override
+    public void addLotto(Lotto lotto) {
+        lottoList.add(lotto);
+    }
+
+    @Override
+    public List<Lotto> getLottoList() {
+        return lottoList;
+    }
+}
