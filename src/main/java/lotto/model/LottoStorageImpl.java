7518c12210a60b7dde81c73e301b297f3036546c
taewook02 <taewook02@mju.ac.kr>
2023-07-20 19:47:08 +0900

    feat: generateLotto 메소드 추가
    로또 생성 메소드

--- src/main/java/lotto/model/LottoStorageImpl.java
+++ src/main/java/lotto/model/LottoStorageImpl.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +9,10 @@ public class LottoStorageImpl implements LottoStorage {
   // TODO: 랜덤 로또 생성 메소드 추가
 
   private final List<Lotto> lottoList = new ArrayList<>();
+
+  public LottoStorageImpl(int lottoQuantity) {
+    generateLotto(lottoQuantity);
+  }
 
   @Override
   public void addLotto(Lotto lotto) {
@@ -16,5 +22,14 @@ public class LottoStorageImpl implements LottoStorage {
   @Override
   public List<Lotto> getLottoList() {
     return lottoList;
+  }
+
+  // TODO: Stream으로 구현하기
+  private void generateLotto(int lottoQuantity) {
+    for (int i = 0; i < lottoQuantity; i++) {
+      List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+      Lotto lotto = new Lotto(numbers);
+      addLotto(lotto);
+    }
   }
 }
