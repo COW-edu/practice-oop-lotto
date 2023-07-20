5afaa4e7d6f096ed50961ae10e063a255f73b7bb
taewook02 <taewook02@mju.ac.kr>
2023-07-20 19:50:42 +0900

    comment: TODO 삭제

--- src/main/java/lotto/model/LottoStorageImpl.java
+++ src/main/java/lotto/model/LottoStorageImpl.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoStorageImpl implements LottoStorage {
-  // TODO: 랜덤 로또 생성 메소드 추가
 
   private final List<Lotto> lottoList = new ArrayList<>();
 
