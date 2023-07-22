cd0a208de25c3fb0c1f691e2b91e4f1882ea2efb
고선제 <koseonje@goseonje-ui-MacBookAir.local>
2023-07-22 22:06:48 +0900

    코드 컨벤션 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,10 @@
 import controller.LottoController;
 
 public class Application {
-    public static void main(String[] args) {
-        LottoAppConfig lottoAppConfig = new LottoAppConfig();
-        LottoController lottoController = lottoAppConfig.lottoController();
-        lottoController.lottoRun();
-    }
+
+  public static void main(String[] args) {
+    LottoAppConfig lottoAppConfig = new LottoAppConfig();
+    LottoController lottoController = lottoAppConfig.lottoController();
+    lottoController.lottoRun();
+  }
 }
