3846960499556d8ae7871dfd77b92d14407bcac5
고선제 <koseonje@goseonje-ui-MacBookAir.local>
2023-07-22 22:02:58 +0900

    주석 제거

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,6 @@ import controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         LottoAppConfig lottoAppConfig = new LottoAppConfig();
         LottoController lottoController = lottoAppConfig.lottoController();
         lottoController.lottoRun();
