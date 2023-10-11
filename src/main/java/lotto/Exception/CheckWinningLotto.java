f533fdc7436bef8e3c6de577a529b3baafa2ef7d
YOON <gkstkddbs99@mju.ac.kr>
2023-10-11 21:29:33 +0900

    fact : 예외처리(금액, 당첨번호, 입력 값) 구현.

---
+++ src/main/java/lotto/Exception/CheckWinningLotto.java
@@ -0,0 +1,28 @@
+package lotto.Exception;
+
+import static lotto.Constant.LottoRule.LOTTO_ROW_SIZE;
+
+public class CheckWinningLotto {
+    //예외처리
+    public static void checkListRange(String[] winningNum) {
+        if (winningNum.length == LOTTO_ROW_SIZE) {
+            return;
+        }
+        try {
+            throw new IllegalAccessException("[ERROR] 로또 번호는 6개를 입력해야합니다.");
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static int checkNumberRange(int number) {
+        if ((number > 0) && (number < 46)) {
+            return number;
+        }
+        try {
+            throw new IllegalAccessException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
