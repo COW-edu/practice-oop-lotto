bc99080728c5db97d20eb798ab6c7ee755eb70b2
woowal <jijile44@gmail.com>
2023-10-16 21:07:15 +0900

    refactor: 로또 번호 중복 검증 위치 변경
    로또 번호 중복 검사를 input view가 아닌 lotto 클래스 내부에서 실행

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -7,12 +7,19 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkDuplication(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void checkDuplication(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다");
         }
     }
 
