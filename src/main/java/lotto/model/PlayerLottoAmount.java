06385be40ae30c37125a0c022592963326f8a5ba
[jina4066] <[jina4066@naver.com]>
2023-10-17 22:20:31 +0900

    refactor: 로또 구입 금액 유효성 검사 기능 리팩토링

--- src/main/java/lotto/model/PlayerLottoAmount.java
+++ src/main/java/lotto/model/PlayerLottoAmount.java
@@ -3,47 +3,45 @@ package lotto.model;
 import lotto.view.ExceptionMessage;
 
 public class PlayerLottoAmount {
-  private static final int LOTTO_MIN_AMOUNT = 1000;
-  private final int amount;
+	private static final int LOTTO_MIN_AMOUNT = 1000;
+	private final int amount;
 
-  public PlayerLottoAmount(String amount) {
-    int amountNum = validateNumber(amount);
-    validateAmount(amountNum);
-    this.amount = amountNum;
-  }
+	public PlayerLottoAmount(String amount) {
+		int amountNum = validateNumber(amount);
+		validateAmount(amountNum);
+		this.amount = amountNum;
+	}
 
-  public int calculateLottoCount() {
-    return amount / LOTTO_MIN_AMOUNT;
-  }
+	public int calculateLottoCount() {
+		return amount / LOTTO_MIN_AMOUNT;
+	}
 
+	private void validateAmount(int amount) {
+		validateNatural(amount);
+		validateDivisible(amount);
+	}
 
-  private void validateAmount(int amount) {
-    validateNatural(amount);
-    validateDivisible(amount);
-  }
+	private int validateNumber(String amount) throws IllegalArgumentException {
+		try {
+			return Integer.valueOf(amount);
+		} catch (NumberFormatException e) {
+			ExceptionMessage.numberException();
+			throw new IllegalArgumentException();
+		}
+	}
 
-  private static int validateNumber(String amount) throws IllegalArgumentException {
-    try {
-      return Integer.parseInt(amount);
-    } catch (NumberFormatException e) {
-      ExceptionMessage.numberException();
-      throw new IllegalArgumentException();
-    }
-  }
+	private void validateNatural(int amount) {
+		if (amount <= 0) {
+			ExceptionMessage.naturalException();
+			throw new IllegalArgumentException();
+		}
+	}
 
-
-  private void validateNatural(int amount) {
-    if (amount <= 0) {
-      ExceptionMessage.naturalException();
-      throw new IllegalArgumentException();
-    }
-  }
-
-  private void validateDivisible(int amount) {
-    if (amount % LOTTO_MIN_AMOUNT != 0) {
-      ExceptionMessage.divisibleException();
-      throw new IllegalArgumentException();
-    }
-  }
+	private void validateDivisible(int amount) {
+		if (amount % LOTTO_MIN_AMOUNT != 0) {
+			ExceptionMessage.divisibleException();
+			throw new IllegalArgumentException();
+		}
+	}
 
 }
