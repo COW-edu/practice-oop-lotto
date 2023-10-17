2c972db410b8c53b7b51570fb72dff200f0b5290
woowal <jijile44@gmail.com>
2023-10-17 12:04:48 +0900

    feat: 보너스 번호 형식 테스트

---
+++ src/test/java/controller/WinningLottoTest.java
@@ -0,0 +1,22 @@
+package controller;
+
+import model.Lotto;
+import model.WinningLotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class WinningLottoTest {
+    @ParameterizedTest
+    @DisplayName("보너스 번호가 잘못되면 예외가 발생한다")
+    @ValueSource(ints = {1, 100})
+    void createLottoByOverBoundary(int bonus) {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6), bonus))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
