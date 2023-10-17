da1b88fa73e04fcac5562781713d1debf9b3857e
[jina4066] <[jina4066@naver.com]>
2023-10-17 22:17:32 +0900

    refactor: 로또 번호 생성 기능 리팩토링

--- src/main/java/lotto/model/GenerateNumbers.java
+++ src/main/java/lotto/model/GenerateNumbers.java
@@ -1,18 +1,18 @@
 package lotto.model;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-public class LottoNumbers {
-  private static final int MIN_LOTTO_NUMBER = 1;
-  private static final int MAX_LOTTO_NUMBER = 45;
-  private static final int CNT_LOTTO_NUMBER = 6;
+import camp.nextstep.edu.missionutils.Randoms;
 
-  public static List<Integer> setRandomNumbers() { //번호만 생성하기
-    List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, CNT_LOTTO_NUMBER);
+public class GenerateNumbers {
+	private static final int MIN_LOTTO_NUMBER = 1;
+	private static final int MAX_LOTTO_NUMBER = 45;
+	private static final int CNT_LOTTO_NUMBER = 6;
 
-    return lottoNumbers;
-  }
+	public static List<Integer> makeRandomNumbers() {
+		List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER,
+			CNT_LOTTO_NUMBER);
+
+		return lottoNumbers;
+	}
 }
