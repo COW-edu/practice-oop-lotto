8c6f820d689205b574a443037dcdeeba82afd861
woowal <jijile44@gmail.com>
2023-10-17 16:23:12 +0900

    refactor: 변수 final 사용
    WinningChecker에서 Grade로

--- src/main/java/controller/WinningLottoFactory.java
+++ src/main/java/controller/WinningLottoFactory.java
@@ -6,13 +6,8 @@ import java.util.Collections;
 import java.util.List;
 
 public class WinningLottoFactory {
-    private List<Integer> sortNumbers(List<Integer> numbers) {
-        Collections.sort(numbers);
-        return numbers;
-    }
 
     public WinningLotto makeWinning(List<Integer> numbers, int bonus) {
-        numbers = sortNumbers(numbers);
         return new WinningLotto(numbers, bonus);
     }
 }
--- src/main/java/model/WinningLotto.java
+++ src/main/java/model/WinningLotto.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class WinningLotto extends Lotto {
 
-    private int bonus;
+    private final int bonus;
 
     public WinningLotto(List<Integer> numbers, int bonus) {
         super(numbers);
