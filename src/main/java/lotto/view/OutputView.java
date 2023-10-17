f1054e49615a668cee4c2e1ef94c9b5eae378f84
[jina4066] <[jina4066@naver.com]>
2023-10-17 22:27:06 +0900

    refactor: 메시지 출력 기능 리팩토링

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,22 +1,33 @@
 package lotto.view;
 
+import java.util.List;
+
+import lotto.model.Ranking;
+
 public class OutputView {
-  public static final String TICKET_COUNT = "개를 구매했습니다.";
 
-  public static void printTicketCount(int count) {
-    System.out.println(count + TICKET_COUNT);
-  }
+	public static void printTicketCount(int count) {
+		System.out.println(count + "개를 구매했습니다.");
+	}
 
-  public static void printSuccessResult() {
-    System.out.println("당첨 통계");
-    System.out.println("---");
-  }
+	public static void printSuccessResult() {
+		System.out.println("당첨 통계");
+		System.out.println("---");
+	}
 
-  public static void printSuccessMessage(String message, int numberOfMatch) {
-    System.out.println(message + numberOfMatch + "개");
-  }
+	public static void printSuccessMessage(String message, int numberOfMatch) {
+		System.out.println(message + numberOfMatch + "개");
+	}
 
-  public static void printRevenueRate(double EarningRate) {
-    System.out.println("총 수익률은 " + String.format("%.1f", EarningRate) + "%입니다.");
-  }
+	public static void printRevenueRate(double EarningRate) {
+		System.out.println("총 수익률은 " + String.format("%.1f", EarningRate) + "%입니다.");
+	}
+
+	public static void printLotto(List<Integer> lottoNumbers) {
+		System.out.println(lottoNumbers);
+	}
+
+	public static void printRankingResult(Ranking rank, Integer count) {
+		printSuccessMessage(rank.getMessage(), count);
+	}
 }
