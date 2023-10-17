8e1b95db45c3213a264a503560f341a24dc46989
[jina4066] <[jina4066@naver.com]>
2023-10-17 22:31:03 +0900

    test: 로또 번호 유효성 검사 기능 테스트 코드 구현

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.model;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -6,8 +6,6 @@ import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import lotto.model.Lotto;
 
 class LottoTest {
 	@DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
