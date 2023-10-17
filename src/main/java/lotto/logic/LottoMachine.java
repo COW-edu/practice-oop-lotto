7e8446e668d6a49f84f8412fb12c2d88a53485f6
YOON <gkstkddbs99@mju.ac.kr>
2023-10-17 20:47:14 +0900

    [Fact]로또 머신 패키지 소문자로 변환

--- src/main/java/lotto/logic/LottoMachine.java
+++ src/main/java/lotto/logic/LottoMachine.java
@@ -1,17 +1,17 @@
-package lotto.Logic;
+package lotto.logic;
 
-import static lotto.Constant.LottoRule.LOTTO_COUNT_NUMBER;
-import static lotto.Constant.LottoRule.LOTTO_FIRST_NUMBER;
-import static lotto.Constant.LottoRule.LOTTO_LAST_NUMBER;
-import static lotto.Constant.LottoRule.LOTTO_ROW_SIZE;
+import static lotto.constant.LottoRule.LOTTO_COUNT_NUMBER;
+import static lotto.constant.LottoRule.LOTTO_MAX_NUMBER;
+import static lotto.constant.LottoRule.LOTTO_MIN_NUMBER;
+import static lotto.constant.LottoRule.LOTTO_ROW_SIZE;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
-import lotto.Entity.Lotto;
+import lotto.entity.Lotto;
 
 public class LottoMachine {
-    // LottoMachine 구현
+    
     private final ArrayList<Lotto> lottoLists;
 
     public LottoMachine() {
@@ -20,8 +20,8 @@ public class LottoMachine {
 
     public ArrayList<Lotto> playLotto(int count) {
         for (int i = 0; i < count; i++) {
-            List<Integer> lottoRow = Randoms.pickUniqueNumbersInRange(LOTTO_FIRST_NUMBER,
-                LOTTO_LAST_NUMBER, LOTTO_COUNT_NUMBER);
+            List<Integer> lottoRow = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER,
+                LOTTO_MAX_NUMBER, LOTTO_COUNT_NUMBER);
             sort(lottoRow);
             Lotto lotto = new Lotto(lottoRow);
             lottoLists.add(lotto);
@@ -29,7 +29,6 @@ public class LottoMachine {
         return lottoLists;
     }
 
-    // 내장 sort 함수로 하면 오류가 나와서 그냥 임의로 만들었습니다.
     public static void sort(List<Integer> lottoRow) {
         for (int i = 0; i < LOTTO_ROW_SIZE; i++) {
             for (int j = i + 1; j < LOTTO_ROW_SIZE; j++) {
