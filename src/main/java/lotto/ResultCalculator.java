bff4f5523ee345c622a1caf9460e6dca7743d859
Suehyun666 <em0200878@gmail.com>
2024-10-05 16:28:26 +0900

    4. 결과계산
    로또 추첨 결과를 처리하는 클래스입니다.

--- src/main/java/lotto/ResultCalculator.java
+++ src/main/java/lotto/ResultCalculator.java
@@ -9,6 +9,26 @@ public class ResultCalculator {
     private static final int MATCH_SIX = 6;
     private static final int MATCH_FIVE_BONUS = 7; // 5개 + 보너스 일치
 
+    private void incrementCount(int[] matchCounts, int index) {
+        if (index >= 0) {
+            matchCounts[index]++;
+        }
+    }
+
+    private int getMatchCount(List<Integer> numbers, List<Integer> winningNumbers) {
+        return (int) numbers.stream().filter(winningNumbers::contains).count();
+    }
+
+    private int calculateResultIndex(int matchCount, boolean bonusMatch) {
+        if (matchCount == MATCH_SIX) {return MATCH_SIX; // 6개 일치
+        } if (matchCount == MATCH_FIVE && bonusMatch) {return MATCH_FIVE_BONUS; // 5개 + 보너스 일치
+        } if (matchCount == MATCH_FIVE) {return MATCH_FIVE; // 5개 일치
+        } if (matchCount == MATCH_FOUR) {return MATCH_FOUR; // 4개 일치
+        } if (matchCount == MATCH_THREE) {return MATCH_THREE; // 3개 일치
+        }return -1; // 일치하지 않는 경우
+
+    }
+
     public int[] calculateResults(List<Lotto> boughtLottos, List<Integer> winningNumbers, int bonusNumber) {
         int[] matchCounts = new int[8];
 
@@ -20,34 +40,5 @@ public class ResultCalculator {
             incrementCount(matchCounts, resultIndex);
         }
         return matchCounts;
-    }
-
-
-    private void incrementCount(int[] matchCounts, int index) {
-        if (index >= 0) {
-            matchCounts[index]++;
-        }
-    }
-
-    private int getMatchCount(List<Integer> numbers, List<Integer> winningNumbers) {
-        return (int) numbers.stream().filter(winningNumbers::contains).count();
-    }
-
-
-
-    private int calculateResultIndex(int matchCount, boolean bonusMatch) {
-        if (matchCount == MATCH_SIX) {
-            return MATCH_SIX; // 6개 일치
-        } else if (matchCount == MATCH_FIVE && bonusMatch) {
-            return MATCH_FIVE_BONUS; // 5개 + 보너스 일치
-        } else if (matchCount == MATCH_FIVE) {
-            return MATCH_FIVE; // 5개 일치
-        } else if (matchCount == MATCH_FOUR) {
-            return MATCH_FOUR; // 4개 일치
-        } else if (matchCount == MATCH_THREE) {
-            return MATCH_THREE; // 3개 일치
-        } else {
-            return -1; // 일치하지 않는 경우
-        }
     }
 }
