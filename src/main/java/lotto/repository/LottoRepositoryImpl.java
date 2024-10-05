6acfb65bcd6035e9c1beae26675ea8bfe2f15a23
Jungsukwoo <jungsw1106@naver.com>
2024-10-05 18:18:20 +0900

    feat: 랜덤 로또 번호 저장소와 당첨 번호 저장소를 분리하여 저장

--- src/main/java/lotto/repository/LottoRepositoryImpl.java
+++ src/main/java/lotto/repository/LottoRepositoryImpl.java
@@ -1,12 +1,14 @@
 package lotto.repository;
 
 import lotto.model.Lotto;
+import lotto.model.WinningNumbers;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoRepositoryImpl implements LottoRepository {
 
-    private final List<Lotto> lottoStorage = new ArrayList<>();
+    private static final List<Lotto> lottoStorage = new ArrayList<>();
+    private static final List<WinningNumbers> winningNumbersStorage = new ArrayList<>();
 
     @Override
     public void saveLotto(Lotto lotto) {
@@ -17,4 +19,10 @@ public class LottoRepositoryImpl implements LottoRepository {
     public List<Lotto> findAllLotto() {
         return new ArrayList<>(lottoStorage);
     }
+
+    @Override
+    public void saveWinningNumbers(WinningNumbers winningNumbers) {
+        winningNumbersStorage.add(winningNumbers); // 당첨 번호 저장
+    }
+
 }
