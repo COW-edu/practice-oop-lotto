0a4199768d64bf36d09ba3c123fc11a923827a40
Jungsukwoo <jungsw1106@naver.com>
2024-10-05 18:16:47 +0900

    feat: 당첨 로또번호 입력 및 저장 메소드 기능 구현

--- src/main/java/lotto/service/LottoServiceImpl.java
+++ src/main/java/lotto/service/LottoServiceImpl.java
@@ -1,29 +1,50 @@
 package lotto.service;
-import camp.nextstep.edu.missionutils.Randoms;
+
 import lotto.model.Lotto;
+import lotto.model.WinningNumbers;
 import lotto.repository.LottoRepository;
-import java.util.ArrayList;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class LottoServiceImpl implements LottoService {
 
     private final LottoRepository lottoRepository;
+    private static final int LOOTO_AMOUNT = 1000;
 
     public LottoServiceImpl(LottoRepository lottoRepository) {
         this.lottoRepository = lottoRepository;
     }
 
     @Override
-    public List<Lotto> purchaseLotto(int amount) {
-        int numberOfLotto = amount / 1000;
-        List<Lotto> purchasedLotto = new ArrayList<>();
+    public void purchaseLottos(int amount) {
+        int numberOfLotto = amount / LOOTO_AMOUNT;
 
-        for (int i = 0; i < numberOfLotto; i++) {
-            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            Lotto lotto = new Lotto(lottoNumbers);
-            purchasedLotto.add(lotto);
+        IntStream.range(0, numberOfLotto).forEach(i -> {
+            Lotto lotto = Lotto.createRandomLotto(); // Lotto도메인에서 팩토리 메소드 호출
+            saveLottoNums(lotto);
+        });
+    }
+
+    @Override
+    public void inputWinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        try {
+            WinningNumbers winningNumbersObj = new WinningNumbers(winningNumbers, bonusNumber);
+            lottoRepository.saveWinningNumbers(winningNumbersObj);
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage();
         }
-        return purchasedLotto;
+    }
+
+    @Override
+    public void saveWinningNumbers(WinningNumbers winningNumbers) {
+        lottoRepository.saveWinningNumbers(winningNumbers);
+    }
+
+    public void saveLottoNums(Lotto lotto){
+        lottoRepository.saveLotto(lotto);
     }
 
 }
