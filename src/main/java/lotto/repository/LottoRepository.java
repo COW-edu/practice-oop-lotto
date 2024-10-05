b3f12088397cd260d273da49033d5101785bb7c4
Jungsukwoo <jungsw1106@naver.com>
2024-10-05 18:17:42 +0900

    feat: 당첨 로또번호 저장 메소드 생성

--- src/main/java/lotto/repository/LottoRepository.java
+++ src/main/java/lotto/repository/LottoRepository.java
@@ -1,9 +1,15 @@
 package lotto.repository;
 
 import lotto.model.Lotto;
+import lotto.model.WinningNumbers;
 import java.util.List;
 
 public interface LottoRepository {
     void saveLotto(Lotto lotto);
+
     List<Lotto> findAllLotto();
+
+    void saveWinningNumbers(WinningNumbers winningNumbers);
+
+//    WinningNumbers findWinningNumbers();
 }
