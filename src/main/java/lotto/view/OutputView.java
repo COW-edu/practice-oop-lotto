37285df55e164acf773c357bf7f3be4893d193f8
Jungsukwoo <jungsw1106@naver.com>
2024-10-05 19:12:29 +0900

    feat: 예외처리 문구 추가 outputvew

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,8 +5,8 @@ public class OutputView {
     private static final String INPUT_LOTTO_PRICE = "구입금액을 입력해 주세요.";
     private static final String INPUT_LOTTO_WINNING = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    private static final String ERROR_MESSAGE = "로또 번호를 6개 입력해 주세요.";
-
+    private static final String ERROR_MESSAGE = "[ERROR] 로또 번호를 6개 입력해 주세요.";
+    private static final String ERROR_INVALID_NUMBER = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 
     public static void printTicketCount(int count) {
         System.out.println(count + TICKET_COUNT);
@@ -35,6 +35,10 @@ public class OutputView {
 
     public static void printErrorMessage() {
         System.out.println(ERROR_MESSAGE);
+    }
+
+    public static void printErrorMessageInvalidNumber() {
+        System.out.println(ERROR_INVALID_NUMBER);
     }
 
     public static void printRevenueRate(double EarningRate) {
