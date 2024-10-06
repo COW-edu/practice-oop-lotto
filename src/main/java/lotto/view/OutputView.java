ed4118bb0525c24ccaaef79f862bbe219930ad5a
Jungsukwoo <jungsw1106@naver.com>
2024-10-06 17:13:53 +0900

    feat: 금액 잘못 넣었을 때 문구 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,6 +5,8 @@ public class OutputView {
     private static final String INPUT_LOTTO_PRICE = "구입금액을 입력해 주세요.";
     private static final String INPUT_LOTTO_WINNING = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String ERROR_MINIMUM_PURCHASE_AMOUNT = "[ERROR] 구입 금액은 최소 1,000원이 필요합니다.";
+    private static final String ERROR_INVALID_PURCHASE_AMOUNT = "[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.";
     private static final String ERROR_MESSAGE = "[ERROR] 로또 번호를 6개 입력해 주세요.";
     private static final String ERROR_INVALID_NUMBER = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 
@@ -31,6 +33,15 @@ public class OutputView {
 
     public static void printInputBonusNumberMessage() {
         System.out.println(INPUT_BONUS_NUMBER);
+
+    }
+
+    public static void printMinimumPurchaseMessage() {
+        System.out.println(ERROR_MINIMUM_PURCHASE_AMOUNT);
+    }
+
+    public static void printInvalidPurchaseMessage() {
+        System.out.println(ERROR_INVALID_PURCHASE_AMOUNT);
     }
 
     public static void printErrorMessage() {
