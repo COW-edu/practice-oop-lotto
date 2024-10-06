004a334de0ab9013b331e050808f8cc3a1c87333
Jungsukwoo <jungsw1106@naver.com>
2024-10-06 23:15:59 +0900

    feat: 모든 구매 로또와 당첨 로또번호를 비교 후 출력 기능 구현

--- src/main/java/lotto/service/LottoServiceImpl.java
+++ src/main/java/lotto/service/LottoServiceImpl.java
@@ -1,6 +1,7 @@
 package lotto.service;
 
 import lotto.model.Lotto;
+import lotto.model.LottoResult;
 import lotto.model.WinningNumbers;
 import lotto.repository.LottoRepository;
 import lotto.view.InputView;
@@ -12,10 +13,12 @@ import java.util.stream.IntStream;
 public class LottoServiceImpl implements LottoService {
 
     private final LottoRepository lottoRepository;
+    private LottoResult lottoResult;
     private static final int LOOTO_AMOUNT = 1000;
 
     public LottoServiceImpl(LottoRepository lottoRepository) {
         this.lottoRepository = lottoRepository;
+        this.lottoResult = new LottoResult();
     }
 
     @Override
@@ -42,18 +45,36 @@ public class LottoServiceImpl implements LottoService {
             lottoRepository.saveWinningNumbers(winningNumbersObj);
 
             outputPurchasedLottos();
+            checkAndPrintResults();
         } catch (IllegalArgumentException e) {
             OutputView.printErrorMessage();
         }
     }
 
-    @Override
-    public void saveWinningNumbers(WinningNumbers winningNumbers) {
-        lottoRepository.saveWinningNumbers(winningNumbers);
-    }
 
     public void saveLottoNums(Lotto lotto){
         lottoRepository.saveLotto(lotto);
     }
+
+    public void checkAndPrintResults() {
+        // 모든 구매 로또와 당첨 번호를 비교하는 메서드
+        List<Lotto> purchasedLottos = lottoRepository.findAllLotto();
+        WinningNumbers winningNumbers = lottoRepository.findWinningNumbers(); // 저장된 당첨 번호 가져오기
+        int matchCount;
+
+        for (Lotto lotto : purchasedLottos) {
+            matchCount = (int) lotto.getNumbers().stream()
+                    .filter(winningNumbers.getNumbers()::contains)
+                    .count();
+
+            boolean hasBonus = lotto.getNumbers().contains(winningNumbers.getBonusNumber());
+            lottoResult.updateResult(matchCount, hasBonus); // 결과 업데이트
+        }
+        lottoResult.printResult(); // 결과 출력
+
+    // 수익률 계산 및 출력
+    double yield = lottoResult.calculateYield(purchasedLottos.size() * LOOTO_AMOUNT);
+    System.out.printf("총 수익률은 %.2f%%입니다.\n", yield);
+}
 
 }
