64269f24d1b945238bb91d5df9102b16f871dc81
Suehyun666 <em0200878@gmail.com>
2024-10-10 01:45:54 +0900

    5.1 당첨번호 저장
    당첨번호를 저장하는 클래스입니다.

---
+++ src/main/java/lotto/WinningInfo.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningInfo {
+
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public WinningInfo(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static WinningInfo fromInput(InputHandler inputHandler) {
+        List<Integer> winningNumbers = inputHandler.getWinningNumbers();
+        int bonusNumber = inputHandler.getBonusNumber(winningNumbers);
+        return new WinningInfo(winningNumbers, bonusNumber);
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
+
