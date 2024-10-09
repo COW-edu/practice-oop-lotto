a6c73907a7e8c7a0c57f3786111a3d4576dd9e9b
Suehyun666 <em0200878@gmail.com>
2024-10-10 01:36:43 +0900

    3.1 로또 클래스
    로또 생성하는 클래스와 합쳤습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,23 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Lotto {
 
     private final List<Integer> numbers;
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6 || isDuplicated(numbers)) {
-            throw new IllegalArgumentException("[ERROR] 잘못된 로또 번호입니다.");
-        }
-    }
-
-    private boolean isDuplicated(List<Integer> numbers) {
-        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-        return uniqueNumbers.size() != numbers.size();
-    }
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -26,5 +17,27 @@ public class Lotto {
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    public static List<Lotto> buy(int money) {
+        int lottoCount = money / LottoConstants.LOTTO_PRICE;
+        return generate(lottoCount);
+    }
+
+    public static List<Lotto> generate(int count) {
+        return IntStream.range(0, count)
+                .mapToObj(i -> new Lotto(
+                        Randoms.pickUniqueNumbersInRange(
+                                LottoConstants.LOTTO_NUMBER_MIN,
+                                LottoConstants.LOTTO_NUMBER_MAX,
+                                LottoConstants.LOTTO_NUMBER_COUNT)
+                ))
+                .collect(Collectors.toList());
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != LottoConstants.LOTTO_NUMBER_COUNT || new HashSet<>(numbers).size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 로또 번호입니다.");
+        }
     }
 }
