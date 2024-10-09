771e99bb3345a5382ee00b94c86f814b8f927469
Suehyun666 <em0200878@gmail.com>
2024-10-10 01:43:39 +0900

    7.1 출력 핸들러
    아직 손볼 곳이 있어서 constants를 만들지 않았습니다.
    상수처리, import, 관련있는 정보 모아서 가독성있게 만들었습니다.

--- src/main/java/lotto/OutputHandler.java
+++ src/main/java/lotto/OutputHandler.java
@@ -1,29 +1,31 @@
 package lotto;
 
-public class OutputHandler {
-    private static final int PRICE_THREE = 5000;
-    private static final int PRICE_FOUR = 50000;
-    private static final int PRICE_FIVE = 1500000;
-    private static final int PRICE_FIVE_BONUS = 30000000;
-    private static final int PRICE_SIX = 2000000000;
+import java.util.List;
+import java.util.Map;
 
-    public void printLottos(java.util.List<Lotto> lottos) {
+public class OutputHandler {
+
+    public void printLottos(List<Lotto> lottos) {
         System.out.println(lottos.size() + "개를 구매했습니다.");
         lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
     }
-    public void printInputPrompt(String message) {
+
+    public void print(Object message) {
         System.out.println(message);
     }
 
-    public void printStatistics(int[] matchCounts, int totalCost) {
-        System.out.println("3개 일치 (5,000원) - " + matchCounts[3] + "개");
-        System.out.println("4개 일치 (50,000원) - " + matchCounts[4] + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + matchCounts[5] + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + matchCounts[7] + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + matchCounts[6] + "개");
+    public void printStatistics(Map<ResultConstants, Integer> resultMap) {
+        System.out.println("당첨 통계");
+        System.out.println("-----------------------------------------");
 
-        double totalPrize = matchCounts[3] * PRICE_THREE + matchCounts[4] * PRICE_FOUR + matchCounts[5] * PRICE_FIVE +  matchCounts[7] * PRICE_FIVE_BONUS + matchCounts[6] * PRICE_SIX;
-        double profitRate = (totalPrize / totalCost) * 100;
+        resultMap.forEach((rank, count) -> {
+            if (count > 0) {
+                System.out.printf("%s (%d원) - %d개%n", rank.getRank(), rank.getPrize(), count);
+            }
+        });
+    }
+
+    public void printProfitRate(double profitRate) {
         System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
     }
 }
