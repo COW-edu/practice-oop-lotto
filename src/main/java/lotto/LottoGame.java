8382b1506d5ae14fef87605aa4c9a661d2422804
Suehyun666 <em0200878@gmail.com>
2024-10-10 01:44:44 +0900

    6.1 로또게임
    의존성을 외부에서 주입하고 주석없앴습니다.
    필드이용하지 않고 정보를 저장하는 클래스를 만들었습니다.
    -클래스이름을 좀 더 이해가기 쉽게 만들고 싶은데 추천부탁드립니다..

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,40 +1,31 @@
 package lotto;
 
-import java.util.List;
+import java.util.Map;
 
 public class LottoGame {
 
     private final InputHandler inputHandler;
-    private final LottoGenerator lottoGenerator;
     private final ResultCalculator resultCalculator;
     private final OutputHandler outputHandler;
 
-    public static final int LOTTO_PRICE = 1000;
-
-    public LottoGame() {
-        this.outputHandler = new OutputHandler();
-        this.inputHandler = new InputHandler(outputHandler);
-        this.lottoGenerator = new LottoGenerator();
-        this.resultCalculator = new ResultCalculator();
+    public LottoGame(InputHandler inputHandler, ResultCalculator resultCalculator, OutputHandler outputHandler) {
+        this.inputHandler = inputHandler;
+        this.resultCalculator = resultCalculator;
+        this.outputHandler = outputHandler;
     }
 
     public void start() {
-        // 구입 금액 입력
-        int money = inputHandler.getMoney();
-        int lottoCount = money / LOTTO_PRICE;
+        PurchaseInfo purchaseinfo = PurchaseInfo.buy(inputHandler.getMoney());
+        outputHandler.printLottos(purchaseinfo.getBoughtLottos());
 
-        // 로또 번호 자동 생성
-        List<Lotto> boughtLottos = lottoGenerator.generateLottos(lottoCount);
-
-        // 구매한 로또 출력
-        outputHandler.printLottos(boughtLottos);
-
-        // 당첨 번호와 보너스 번호 입력
-        List<Integer> winningNumbers = inputHandler.getWinningNumbers();
-        int bonusNumber = inputHandler.getBonusNumber(winningNumbers);
-
-        // 당첨 결과 계산 및 출력
-        int[] matchCounts = resultCalculator.calculateResults(boughtLottos, winningNumbers, bonusNumber);
-        outputHandler.printStatistics(matchCounts, lottoCount * LOTTO_PRICE);
+        WinningInfo winningInfo = WinningInfo.fromInput(inputHandler);
+        Map<ResultConstants, Integer> result = resultCalculator.calculateResults(
+                purchaseinfo.getBoughtLottos(),
+                winningInfo.getWinningNumbers(),
+                winningInfo.getBonusNumber()
+        );
+        outputHandler.printStatistics(result);
+        outputHandler.printProfitRate(resultCalculator.calculateProfitRate(result, purchaseinfo.getMoney()));
     }
 }
+
