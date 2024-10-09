3449189e38775bcaae48da2a4f0c7239a510d3c0
Jungsukwoo <jungsw1106@naver.com>
2024-10-09 23:55:38 +0900

    feat: 매직넘버를 상수로 선언, Enum으로 바꾼 에러 메시지 호출 메소드 수정

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,13 +1,18 @@
 package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.view.ErrorMessage;
 import lotto.view.OutputView;
 import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers; //로또 갯수 저장필드
     private static final int LOOTO_AMOUNT = 1000;
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 1;
+    private static final int COUNT = 1;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -15,7 +20,7 @@ public class Lotto {
     }
 
     public static Lotto createRandomLotto() {
-        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT);
         Collections.sort(lottoNumbers);
         return new Lotto(lottoNumbers);
     }
@@ -25,23 +30,23 @@ public class Lotto {
 
     public static void validatePurchaseAmount(int amount) {
         if (amount < LOOTO_AMOUNT) {
-            OutputView.printMinimumPurchaseMessage();
+            OutputView.printErrorMessage(ErrorMessage.ERROR_MINIMUM_PURCHASE_AMOUNT);
             throw new IllegalArgumentException();
         }
         if (amount % LOOTO_AMOUNT != 0) {
-            OutputView.printInvalidPurchaseMessage();
+            OutputView.printErrorMessage(ErrorMessage.ERROR_INVALID_PURCHASE_AMOUNT);
             throw new IllegalArgumentException();
         }
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            OutputView.printErrorMessage();
+            OutputView.printErrorMessage(ErrorMessage.ERROR_MESSAGE);
             throw new IllegalArgumentException();
         }
         for (Integer number : numbers) {
             if (number < 1 || number > 45) {
-                OutputView.printErrorMessageInvalidNumber();
+                OutputView.printErrorMessage(ErrorMessage.ERROR_INVALID_NUMBER);
                 throw new IllegalArgumentException();
             }
         }
