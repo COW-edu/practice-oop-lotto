a5440df36619fbe28f6b3063d6a5793b3944e22b
Suehyun666 <em0200878@gmail.com>
2024-10-10 01:37:01 +0900

    3.2 lottoconstants
    로또 상수처리 위해 constants만들었습니다.

---
+++ src/main/java/lotto/LottoConstants.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class LottoConstants {
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+}
+
