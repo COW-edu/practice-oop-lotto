6de8f99ba369a592b04c370680782eb6516603b3
Suehyun666 <em0200878@gmail.com>
2024-10-10 01:41:40 +0900

    4.3 매치카운트 정보
    매치카운트를 저장하는 클래스입니다.

---
+++ src/main/java/lotto/MatchResult.java
@@ -0,0 +1,19 @@
+package lotto;
+
+public class MatchResult {
+    private final int matchCount;
+    private final boolean bonusMatch;
+
+    public MatchResult(int matchCount, boolean bonusMatch) {
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isBonusMatch() {
+        return bonusMatch;
+    }
+}
