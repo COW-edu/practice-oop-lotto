3430382e25b3bce161fe9ccc3feaa81110d3b3af
Suehyun666 <em0200878@gmail.com>
2024-10-10 12:04:11 +0900

    2.1 inputhandler
    바뀐 곳은 별로 없습니다.
    String input = console.realine이 반복돼서 줄일까 합니다.

---
+++ src/main/java/lotto/service/InputHandler.java
@@ -0,0 +1,58 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.utils.InputConstants;
+import lotto.utils.InputValidator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputHandler {
+
+    private final OutputHandler outputHandler;
+    private final InputValidator inputValidator;
+
+    public InputHandler(OutputHandler outputHandler, InputValidator inputValidator) {
+        this.outputHandler = outputHandler;
+        this.inputValidator = inputValidator;
+    }
+
+    public int getMoney() {
+        outputHandler.print(InputConstants.GET_MONEY_PROMPT.getMessage());
+        String input = Console.readLine();
+        inputValidator.validateMoney(input);
+        return Integer.parseInt(input);
+    }
+
+    public List<Integer> getWinningNumbers() {
+        outputHandler.print(InputConstants.GET_WINNING_PROMPT.getMessage());
+        String input = Console.readLine();
+        return parseNumbers(input);
+    }
+
+    public int getBonusNumber(List<Integer> winningNumbers) {
+        outputHandler.print(InputConstants.GET_BONUS_PROMPT.getMessage());
+        String input = Console.readLine();
+        return parseBonusNumber(input, winningNumbers);
+    }
+
+    private List<Integer> parseNumbers(String input) {
+        String[] tokens = input.split(InputConstants.PARSING_CRITERIA.getMessage());
+        inputValidator.validateWinningCount(tokens);
+
+        List<Integer> numbers = new ArrayList<>();
+        for (String token : tokens) {
+            int number = Integer.parseInt(token.trim());
+            inputValidator.validateRange(number);
+            numbers.add(number);
+        }
+
+        inputValidator.validateNoDupes(numbers);
+        return numbers;
+    }
+
+    private int parseBonusNumber(String input, List<Integer> winningNumbers) {
+        int bonusNumber = Integer.parseInt(input.trim());
+        inputValidator.validateBonus(bonusNumber, winningNumbers);
+        return bonusNumber;
+    }
+}
