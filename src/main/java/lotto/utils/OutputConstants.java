dfb20a111fcf8a46357e21332ccd51808af133fa
Suehyun666 <em0200878@gmail.com>
2024-10-10 12:18:22 +0900

    7.2.1 outputconstants
    constants입니다.

---
+++ src/main/java/lotto/utils/OutputConstants.java
@@ -0,0 +1,10 @@
+package lotto.utils;
+
+public class OutputConstants {
+    public static final String BoughtCount_PROMPT = "개를 구매했습니다.";
+    public static final String STATISTIC_PROMPT = "당첨 통계";
+    public static final String PARSING = "-----------------------------------------";
+    public static final String RESULTCOUNT = "%s (%d원) - %d개%n";
+    public static final String PROFITRATE = "총 수익률은 %.1f%%입니다.%n";
+
+}
