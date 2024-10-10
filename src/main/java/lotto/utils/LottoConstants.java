2aa1c6f2178bedf9aa35ca6b5f1905df63a6a630
Suehyun666 <em0200878@gmail.com>
2024-10-10 12:06:24 +0900

    3.2.1 lottoconstants
    바뀐 곳은 없습니다.

---
+++ src/main/java/lotto/utils/LottoConstants.java
@@ -0,0 +1,10 @@
+package lotto.utils;
+
+public class LottoConstants {
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final String LOTTO_ERROR = "[ERROR] 잘못된 로또 번호입니다.";
+}
+
