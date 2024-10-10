96bbaf30e3ca18753e87bf773f0002cacfb2c3e6
Suehyun666 <em0200878@gmail.com>
2024-10-10 12:14:39 +0900

    4.2.1 resultconstants
    바뀐 곳은 별로 없습니다.
    상수를 담고있는 costants입니다.

---
+++ src/main/java/lotto/utils/ResultConstants.java
@@ -0,0 +1,43 @@
+package lotto.utils;
+
+public enum ResultConstants {
+    FIRST(6, false, 2_000_000_000, "1등"),
+    SECOND(5, true, 30_000_000, "2등"),
+    THIRD(5, false, 1_500_000, "3등"),
+    FOURTH(4, false, 50_000, "4등"),
+    FIFTH(3, false, 5_000, "5등"),
+    NO_MATCH(0, false, 0, "낙첨");
+
+    private final int matchCount;
+    private final boolean bonusMatch;
+    private final int prize;
+    private final String rank;
+
+    public static final int PERCENTAGE_CONVERTER = 100;
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+
+    ResultConstants(int matchCount, boolean bonusMatch, int prize, String rank) {
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+        this.prize = prize;
+        this.rank = rank;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public static ResultConstants valueOf(int matchCount, boolean bonusMatch) {
+        for (ResultConstants resultConstants : values()) {
+            if (resultConstants.matchCount == matchCount && resultConstants.bonusMatch == bonusMatch) {
+                return resultConstants;
+            }
+        }
+        return NO_MATCH;
+    }
+}
