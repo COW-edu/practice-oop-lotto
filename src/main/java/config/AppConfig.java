e7f18d7cd45ecd33afed24811f8adce01abb99d8
xxxjinn <tlsguswls319@naver.com>
2025-03-29 15:22:28 +0900

    feat: 구입 금액 입력받기 구현

---
+++ src/main/java/config/AppConfig.java
@@ -0,0 +1,9 @@
+package config;
+
+import view.InputView;
+
+public class AppConfig {
+    public InputView inputView(){
+        return new InputView();
+    }
+}
--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import config.AppConfig;
+import view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        AppConfig config = new AppConfig();
+        InputView inputView = config.inputView();
+        inputView.getPurchaseAmount();
     }
 }
