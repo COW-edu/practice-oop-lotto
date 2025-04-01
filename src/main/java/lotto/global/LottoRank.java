fa19757fc690c6e2e9cf121bbefcd70478655c86
bapdodi <arlawjdqls12@gmail.com>
2025-04-01 14:38:05 +0900

    refactor : enum 반환 수정
    enum 반환을 null이 아닌 Optional로 수정했습니다

--- src/main/java/lotto/global/LottoRank.java
+++ src/main/java/lotto/global/LottoRank.java
@@ -1,42 +1,44 @@
 package lotto.global;
 
+import java.util.Optional;
+
 public enum LottoRank {
 
-    FIFTH(3,false,5000, "3개 일치 (5,000원)"),
-    FOURTH(4,false,50000, "4개 일치 (50,000원)"),
-    THIRD(5,false,1500000, "5개 일치 (1,500,000원)"),
-    SECOND(5,true,30000000, "5개, 보너스 볼 일치 (30,000,000원)"),
-    FIRST(6,false,2000000000, "6개 일치 (200,000,000원)");
+    FIFTH(3, false, 5_000, "3개 일치 (5,000원)"),
+    FOURTH(4, false, 50_000, "4개 일치 (50,000원)"),
+    THIRD(5, false, 1_500_000, "5개 일치 (1,500,000원)"),
+    SECOND(5, true, 30_000_000, "5개, 보너스 볼 일치 (30,000,000원)"),
+    FIRST(6, false, 200_000_000, "6개 일치 (200,000,000원)");
 
-    private final int count;
+    private final int matchCount;
     private final boolean bonus;
     private final int prize;
     private final String prizeText;
 
-    
-    LottoRank(int count, boolean bonus,int prize, String prizeText) {
-        this.count = count;
+
+    LottoRank(int matchCount, boolean bonus, int prize, String prizeText) {
+        this.matchCount = matchCount;
         this.bonus = bonus;
         this.prize = prize;
         this.prizeText = prizeText;
     }
-    
-    public static LottoRank valueOf(int count, boolean bonus) {
-        if(count== SECOND.count && bonus == SECOND.bonus) {
-            return SECOND;
+
+    public static Optional<LottoRank> valueOf(int matchCount, boolean bonus) {
+        if (matchCount == SECOND.matchCount && bonus == SECOND.bonus) {
+            return Optional.of(SECOND);
         }
-        for(LottoRank rank : values()) {
-            if(rank.count == count) {
-                return rank;
+        for (LottoRank rank : values()) {
+            if (rank.matchCount == matchCount) {
+                return Optional.of(rank);
             }
         }
-        return null;
+        return Optional.empty();
     }
 
-    public int getCount() {
-        return count;
+    public int getMatchCount() {
+        return matchCount;
     }
-    
+
     public int getPrize() {
         return prize;
     }
@@ -45,3 +47,7 @@ public enum LottoRank {
         return prizeText;
     }
 }
+
+
+
+
