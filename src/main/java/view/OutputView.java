a799308167074828cf14f53ee55d7ec10a9264e4
Hyemin Seo <hhese89985@gmail.com>
2025-04-05 22:46:32 +0900

    refactor: 출력 클래스 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,7 +1,9 @@
 package view;
 
-import model.Lotto;
+import model.Rank;
 import java.util.List;
+import java.util.Map;
+import model.LottoResult;
 
 public class OutputView {
 
@@ -11,4 +13,18 @@ public class OutputView {
             System.out.println(numbers);
         }
     }
+
+        public static void printWinningStatistics(LottoResult result) {
+            System.out.println("\n당첨 통계");
+            System.out.println("---");
+
+            Map<Rank, Integer> results = result.getResults();
+            System.out.printf("3개 일치 (5,000원) - %d개%n", results.get(Rank.FIFTH));
+            System.out.printf("4개 일치 (50,000원) - %d개%n", results.get(Rank.FOURTH));
+            System.out.printf("5개 일치 (1,500,000원) - %d개%n", results.get(Rank.THIRD));
+            System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개%n", results.get(Rank.SECOND));
+            System.out.printf("6개 일치 (2,000,000,000원) - %d개%n", results.get(Rank.FIRST));
+
+            System.out.println("총 수익률은 " + result.calculateProfitRate() + "%입니다.");
+        }
 }
