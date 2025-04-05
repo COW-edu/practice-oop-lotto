5910156514862a5befcd5f6d3b512d9182a4879b
Hyemin Seo <hhese89985@gmail.com>
2025-04-05 22:42:41 +0900

    refactor: 메소드 위치 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,33 +5,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
+    private final static String PURCHASE_MESSAGE = "구매금액을 입력해주세요.";
+    private final static String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해주세요.";
+    private final static String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     //구매금액
     public static int inputPrice() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(PURCHASE_MESSAGE);
         return Integer.parseInt(Console.readLine());
     }
 
     //당첨번호
-    public static List<Integer> inputWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String input = Console.readLine();
-        return parseNumbers(input);
+    public static String inputWinningNumbers() {
+        System.out.println(WINNING_NUMBERS_MESSAGE);
+        return Console.readLine();
     }
 
     //보너스 번호
     public static int inputBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(BONUS_NUMBER_MESSAGE);
         return Integer.parseInt(Console.readLine());
-    }
-
-    //문자열 정수 변환
-    private static List<Integer> parseNumbers(String number) {
-        List<Integer> numbers = new ArrayList<>();
-        String[] numberStrings = number.split(",");
-        for (String numberString : numberStrings) {
-            numbers.add(Integer.parseInt(numberString.trim()));
-        }
-        return numbers;
     }
 }
