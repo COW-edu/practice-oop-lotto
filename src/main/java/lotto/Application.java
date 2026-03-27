87d5ea09579c03c62052301c1695d7e1e9c8dc02
BXXNXXII <pgb0511@naver.com>
2026-03-28 02:51:52 +0900

    feat(application): 로또 실행 흐름 구성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,52 @@
 package lotto;
 
+import lotto.domain.LottoIssuer;
+import lotto.domain.LottoResultCalculator;
+import lotto.domain.LottoStatistics;
+import lotto.domain.WinningLotto;
+import lotto.parser.InputParser;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            run();
+        } catch (IllegalArgumentException exception) {
+            OutputView.printError(exception.getMessage());
+        }
+    }
+
+    private static void run() {
+        int purchaseAmount = readPurchaseAmount();
+        List<Lotto> purchasedLottos = issueLottos(purchaseAmount);
+        WinningLotto winningLotto = readWinningLotto();
+        LottoStatistics statistics = calculateStatistics(purchasedLottos, winningLotto);
+        OutputView.printStatistics(statistics, statistics.calculateProfitRate(purchaseAmount));
+    }
+
+    private static int readPurchaseAmount() {
+        return InputParser.parsePurchaseAmount(InputView.readPurchaseAmount());
+    }
+
+    private static List<Lotto> issueLottos(int purchaseAmount) {
+        LottoIssuer lottoIssuer = new LottoIssuer();
+        List<Lotto> purchasedLottos = lottoIssuer.issue(purchaseAmount);
+        OutputView.printPurchasedLottos(purchasedLottos);
+        return purchasedLottos;
+    }
+
+    private static WinningLotto readWinningLotto() {
+        List<Integer> winningNumbers = InputParser.parseWinningNumbers(InputView.readWinningNumbers());
+        Lotto winningLotto = new Lotto(winningNumbers);
+        int bonusNumber = InputParser.parseBonusNumber(InputView.readBonusNumber());
+        return new WinningLotto(winningLotto, bonusNumber);
+    }
+
+    private static LottoStatistics calculateStatistics(List<Lotto> purchasedLottos, WinningLotto winningLotto) {
+        LottoResultCalculator calculator = new LottoResultCalculator();
+        return calculator.calculate(purchasedLottos, winningLotto);
     }
 }
