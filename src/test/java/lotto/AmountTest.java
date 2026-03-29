7d41fba46ad42796d85ff66929ce264dc60d70b9
KSG44 <gon040422@naver.com>
2026-03-29 14:16:42 +0900

    refactor: Amount 클래스에 예외 메시지 Enum 적용

--- src/test/java/lotto/AmountTest.java
+++ src/test/java/lotto/AmountTest.java
@@ -1,5 +1,6 @@
-package domain;
+package lotto;
 
+import domain.Amount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
