d545550a82e6c64223ec294e59729fa5226e65b7
abins <iamabins@naver.com>
2026-03-31 12:52:28 +0900

    feat: 당첨 통계 출력 기능

--- src/main/java/kr/abins/lotto/view/LottoStatistics.java
+++ src/main/java/kr/abins/lotto/view/LottoStatistics.java
@@ -9,8 +9,30 @@ public interface LottoStatistics {
         System.out.println("당첨 통계");
         System.out.println("---");
 
+        printLottoPrizes(result);
+        printProfitRate(result);
     }
 
+    private static void printLottoPrizes(final LottoResult result) {
+        final String results = String.format(
+            "3개 일치 (%s) - %d개\n" +
+            "4개 일치 (%s) - %d개\n" +
+            "5개 일치 (%s) - %d개\n" +
+            "5개 일치, 보너스 볼 일치 (%s) - %d개\n" +
+            "6개 일치 (%s) - %d개",
+            money(LottoPrize.PRIZE_5TH), result.getCount(LottoPrize.PRIZE_5TH),
+            money(LottoPrize.PRIZE_4TH), result.getCount(LottoPrize.PRIZE_4TH),
+            money(LottoPrize.PRIZE_3RD), result.getCount(LottoPrize.PRIZE_3RD),
+            money(LottoPrize.PRIZE_2ND), result.getCount(LottoPrize.PRIZE_2ND),
+            money(LottoPrize.PRIZE_1ST), result.getCount(LottoPrize.PRIZE_1ST)
+        );
+        System.out.println(results);
+    }
+
+    private static void printProfitRate(final LottoResult result) {
+        double profitRatePercent = result.calculateProfitRate();
+        System.out.printf("총 수익률은 %,.1f%%입니다.%n", profitRatePercent);
+    }
 
     private static String money(final LottoPrize prize) {
         final long money = prize.prizeMoney();
