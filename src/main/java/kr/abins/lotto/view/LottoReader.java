547cb05a0bac64015c39f6acd30af35774b6a38b
abins <iamabins@naver.com>
2026-03-31 12:38:09 +0900

    feat: 구매 금액 입력 기능

---
+++ src/main/java/kr/abins/lotto/view/LottoReader.java
@@ -0,0 +1,22 @@
+package kr.abins.lotto.view;
+
+import kr.abins.lotto.Constants;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public interface LottoReader extends Reader {
+    static int readPurchaseAmount() throws IllegalArgumentException {
+        return Reader.readInt(number -> {
+            if (number <= 0) {
+                throw new IllegalArgumentException(Constants.ERROR_PURCHASE_AMOUNT_NOT_POSITIVE);
+            }
+            if (number % 1000 == 0) {
+                return;
+            }
+
+            throw new IllegalArgumentException(Constants.ERROR_PURCHASE_AMOUNT_NOT_DIVISIBLE);
+        });
+    }
+}
---
+++ src/main/java/kr/abins/lotto/view/LottoWriter.java
@@ -0,0 +1,6 @@
+package kr.abins.lotto.view;
+public interface LottoWriter {
+    static void printPurchaseAmountMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+}
