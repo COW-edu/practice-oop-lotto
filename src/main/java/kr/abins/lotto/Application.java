fdd43c2f7e18006e3409a303fd2eba91eefb1e2e
abins <iamabins@naver.com>
2026-03-31 12:52:39 +0900

    feat: 예외 상황 발생 시 [ERROR] 메시지 출력 후 종료 기능

---
+++ src/main/java/kr/abins/lotto/Application.java
@@ -0,0 +1,27 @@
+package kr.abins.lotto;
+
+import kr.abins.lotto.model.LottoResult;
+import kr.abins.lotto.view.LottoReader;
+import kr.abins.lotto.controller.LottoSession;
+import kr.abins.lotto.view.LottoStatistics;
+
+public class Application {
+
+    public static void main(String[] args) {
+        try {
+            main0();
+        } catch (final RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main0() throws RuntimeException {
+        // 의존성 주입 패턴이라면 LottoReader를 인터페이스로 안만들고 객체화 했을 것 같습니다.
+        // 제가 생각하기에 상태를 가질 이유가 없어 인터페이스로 객체화없이 구현했습니다.
+        final LottoSession session = new LottoSession();
+        session.start();
+
+        final LottoResult result = new LottoResult(session.recipes(), session.purchaseAmount());
+        LottoStatistics.print(result);
+    }
+}
