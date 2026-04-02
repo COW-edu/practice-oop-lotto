2f7e3c9a89a1b4a8427f8f5c40b56f22a50511f6
abeansu <a01033578398@gmail.com>
2026-04-02 15:36:42 +0900

    fix: 코드를 간결하게 만듬

--- src/main/java/kr/abins/lotto/model/LottoResult.java
+++ src/main/java/kr/abins/lotto/model/LottoResult.java
@@ -1,38 +1,40 @@
 package kr.abins.lotto.model;
 
-import java.util.HashMap;
+import kr.abins.lotto.Constants;
+
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LottoResult {
+
     private final Map<LottoPrize, Integer> prizeCounts;
     private final int purchaseAmount;
 
-    public LottoResult(Map<LottoPrize, List<LottoRecipe>> recipes, int purchaseAmount) {
-        this.prizeCounts = new HashMap<>();
-        for (LottoPrize prize : LottoPrize.values()) {
-            this.prizeCounts.put(prize, recipes.getOrDefault(prize, List.of()).size());
-        }
-        this.purchaseAmount = purchaseAmount;
+    public LottoResult(final Map<LottoPrize, List<LottoRecipe>> recipes) {
+        this.prizeCounts = recipes.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().size()));
+        this.purchaseAmount = recipes.values().stream().mapToInt(List::size).sum() * Constants.LOTTO_PRICE;
     }
 
-    public int getCount(LottoPrize prize) {
-        return prizeCounts.getOrDefault(prize, 0);
+    public int count(LottoPrize prize) {
+        return this.prizeCounts.getOrDefault(prize, 0);
     }
 
     public double calculateProfitRate() {
-        if (purchaseAmount == 0) {
+        if (this.purchaseAmount == 0) {
             return 0;
         }
-        long totalPrizeMoney = calculateTotalPrizeMoney();
-        return (double) totalPrizeMoney / purchaseAmount * 100;
+
+        final long totalPrizeMoney = this.calculateTotalPrizeMoney();
+        return (((double) totalPrizeMoney) / this.purchaseAmount) * 100;
     }
 
     private long calculateTotalPrizeMoney() {
         long total = 0;
-        for (LottoPrize prize : LottoPrize.values()) {
-            total += prize.prizeMoney() * getCount(prize);
+        for (final LottoPrize prize : LottoPrize.values()) {
+            total += prize.prizeMoney() * this.count(prize);
         }
+
         return total;
     }
 }
