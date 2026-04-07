7c1baa9ff3cbb6107cfc7d9e405c0ffa3a207d2c
Ihyeon <wozifs05@gmail.com>
2026-04-08 00:11:23 +0900

    feat: LottoRank 내 보너스 볼 판정 필드 및 로직 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,34 +1,32 @@
 package lotto.domain;
 
 public enum LottoRank {
-    FIFTH(3, 5000, "3개 일치 (5,000원) - "),
-    FOURTH(4, 50000, "4개 일치 (50,000원) - "),
-    THIRD(5, 1500000, "5개 일치 (1,500,000원) - "),
-    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - "),
-    NONE(0, 0, "");
+    FIRST(6, false, 2000000000, "6개 일치 (2,000,000,000원) - "),
+    SECOND(5, true, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(5, false, 1500000, "5개 일치 (1,500,000원) - "),
+    FOURTH(4, false, 50000, "4개 일치 (50,000원) - "),
+    FIFTH(3, false, 5000, "3개 일치 (5,000원) - "),
+    NONE(0, false, 0, "");
 
     private final int matchCount;
+    private final boolean matchBonus;
     private final int prize;
     private final String message;
 
-    LottoRank(int matchCount, int prize, String message) {
+    LottoRank(int matchCount, boolean matchBonus, int prize, String message) {
         this.matchCount = matchCount;
+        this.matchBonus = matchBonus;
         this.prize = prize;
         this.message = message;
     }
 
-    public static LottoRank getRank(int matchCount, boolean bonus) {
+    public static LottoRank getRank(int matchCount, boolean matchbonus) {
         if (matchCount == 6) return FIRST;
-        if (matchCount == 5&& bonus) return SECOND;
+        if (matchCount == 5 && matchbonus) return SECOND;
         if (matchCount == 5) return THIRD;
         if (matchCount == 4) return FOURTH;
         if (matchCount == 3) return FIFTH;
         return NONE;
-    }
-
-    public int getMatchCount() {
-        return matchCount;
     }
 
     public int getPrize() {
